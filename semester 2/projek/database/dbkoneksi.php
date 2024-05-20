9db1605896014c9287c15383bfdafee3c32ba420
shafhan <shafhan.fa@gmail.com>
2024-05-20 15:06:08 +0700

    Menghapus Koneksi Database Lama Menjadi Database Kreasi

--- semester 2/projek/database/dbkoneksi.php
+++ semester 2/projek/database/dbkoneksi.php
@@ -1,14 +1,9 @@
 <?php
 
-// $host = 'sql6.freesqldatabase.com';
-// $db = 'sql6705327';
-// $user = 'sql6705327';
-// $pass = 'YpbUG1mDkJ';
-$host = '127.0.0.1';
-$db = 'db_shaf23019ti';
-$user = 'shaf23019ti';
-$pass = '22510110223019';
-
+$host = 'sql6.freesqldatabase.com';
+$db = 'sql6705327';
+$user = 'sql6705327';
+$pass = 'YpbUG1mDkJ';
 $charset = 'utf8mb4';
 
 $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
